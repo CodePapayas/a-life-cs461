a97c7cc91033aa13e1c64d3279b123a34e948d8c
CodePapayas <catspianos@gmail.com>
2026-01-20 22:54:22 -0800

    Create activation layer functions and dot_product function; create decision center folder and associated tests

---
+++ decision_center/brain.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Activation layer equations
+double relu(double x);
+double tanh_func(double x);
+double sigmoid(double x);
+double dot_product(std::vector<double> v1, std::vector<double> v2);
+
+// Layer functions
+double ActivationLayerRelu(double x);
