b8144ab0f0bce93cde167d43ee97a601d0b7b06b
NopeOfAllNopes <78011942+NopeOfAllNopes@users.noreply.github.com>
2026-02-06 02:41:31 -0800

    Environment-testing to Main (#5)
    * Added main.cpp, environment.hpp, environment.cpp
    
    * remembered I should use the github, moved test code over from notepad++
    
    * fixed array size declaration issue
    
    * started process of separation into proper header and source files, renamed header file
    
    * fixed improper header file access
    
    * Formatted main.cpp, added constructor for environment, moved chunk_amt and tile_amt, added comments for clarity to environment.cpp and .h
    
    * added getTileInfo(), setup constructer to initialize 'chunks' 2d array
    
    * started adding code to main for visual tests, added overloads to environment functions to take both int parameters and Vector2d parameters
    
    * Moved header and source file together for linker issues on local end, issues with chunk-tile list initialization
    
    * fixed a really goofy mistake, was accounting for something I hadn't implemented yet.
    
    * Oh also I changed the chunks and tiles arrays to vectors in the last commit, makes things more configurable
    
    * Cleanup
    
    * cleanup of folder names, removed header folder
    
    * further cleanup
    
    * Cleanup and comments for clarity and planning, moved visual testing code to a separate function in main
    
    * fixed issue with improper tile printing, need to add better out-of-bound accessing preventions
    
    * gave tiles a normalized float value for placeholder testing, cleaned up test display of environment, redid class variables for abstraction
    
    * Got deconstructors setup, need to do some extra unit testing but is done... for now.
    
    * Moved perlin noise script from local space to repository space, added credentials.
    
    * Moved fixed perlin noise sampler from local to repo
    
    * comment cleanup
    
    * further code and comment cleanup
    
    * Extra documentation on functions
    
    * added vector coord class, perlin noise class, and hash map to the environment. Perlin noise needs fixing.
    
    * removed old files
    
    * Fixed getTileValue() indexing issue, added extra tests
    
    * Testing on chunkID retrieval from unordered hash map
    
    ---------
    
    Co-authored-by: Dillon Stickler <stickldi@oregonstate.edu>

---
+++ source/PerlinNoise.hpp
@@ -0,0 +1,139 @@
+/*
+ *  Dillon Stickler - Oregon State University - 2026
+ */
+
+#pragma once
+#ifndef PERLINNOISE_H
+#define PERLINNOISE_H
+
+#include <cmath>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <cstring>
+#include <time.h>
+#include "MathVector.hpp"
+
+/* 
+   Perlin Noise function, 
+   adapted from Ken Perlin's "JAVA REFERENCE IMPLEMENTATION OF IMPROVED NOISE",
+   https://cs.nyu.edu/~perlin/noise/
+   and Raouf Touti's adaptation of Ken Perlin's function,
+   https://rtouti.github.io/graphics/perlin-noise-algorithm
+*/
+class PerlinNoise2d{
+	private:
+		std::vector<double> p; //permutation table
+		int p_table_size = 256;
+
+		int octaves = 8;
+		double frequency = 1;
+		double amplitude = 1;
+		
+	public:		
+		double lerp(double a1, double a2, double t) {return (a1 + t * (a2 - a1));}
+		double fade(double t) 						{return ((6.0 * t - 15.0) * t + 10.0) * t * t * t ;}
+		
+		void shuffle(){
+			int shuffle_times = 1;
+			for(int s = 0; s < shuffle_times; s++){
+				for(int i = p.size()-1; i > 0; i--){
+					int index = round(rand() * i-1);
+					if(index < 0){std::cout << "ERROR OUT OF BOUNDS" << std::endl;}
+					index = index % p.size();
+					double temp = p[i];
+					
+					p[index] = p[i];
+					p[i] = temp;
+				}
+			}
+		}
+		
+		void makePermutation(){
+			for(int i = 0; i < p_table_size; i++){
+				p.push_back((double)i);
+			}
+			shuffle();
+			
+			for(int i = 0; i < p_table_size; i++){
+				p.push_back(p[i]);
+			}
+			
+		}
+		
+		Vector2d toConstantVector(double v){
+			int h = (int)fmod(v, 4.0);
+			switch (h){
+				case 0:
+					return Vector2d(1.0, 1.0);
+				case 1:
+					return Vector2d(-1.0, 1.0);
+				case 2:
+					return Vector2d(-1.0, -1.0);
+				case 3:
+					return Vector2d(1.0, -1.0);
+				default:
+					return Vector2d(0.0, 0.0);
+			}
+		}
+	
+		double SampleNoiseBase(double x, double y){
+			
+			x = x * (1.1);
+			y = y * (1.1);
+
+			int X = x;
+			X = X % p_table_size;
+			int Y = y;
+			Y = Y % p_table_size;
+
+
+			double xf = x - floor(x);
+			double yf = y - floor(y);
+
+			Vector2d tr_v = Vector2d(xf - 1.0, yf - 1.0);
+			Vector2d tl_v = Vector2d(xf, yf - 1.0);
+			Vector2d br_v = Vector2d(xf - 1.0, yf);
+			Vector2d bl_v = Vector2d(xf, yf);
+
+			double val_tr = p[p[X + 1] + Y + 1];
+			double val_tl = p[p[X] + Y + 1];
+			double val_br = p[p[X + 1] + Y];
+			double val_bl = p[p[X] + Y];
+
+			double dot_tr = tr_v.dot(toConstantVector(val_tr));
+			double dot_tl = tl_v.dot(toConstantVector(val_tl));
+			double dot_br = br_v.dot(toConstantVector(val_br));
+			double dot_bl = bl_v.dot(toConstantVector(val_bl));
+
+			double u = fade(xf);
+			double v = fade(yf);
+
+			double result = lerp(lerp(dot_bl, dot_tl, v),lerp( dot_br, dot_tr, v), u);
+
+			return result;
+		}
+		
+		// gets noise value at position, modified by the frequency, amplitude and octaves for a more detailed overall result
+		double SampleNoiseLayered(Vector2d pos){
+			double curr_sample 	= 0;
+			double curr_freq 	= frequency;
+			double curr_amp 	= amplitude;
+			for(int o = 1; o < octaves; o++){
+				curr_sample += curr_amp * SampleNoiseBase(curr_freq * pos.x, curr_freq * pos.y);
+				curr_amp *= 0.5;
+				curr_freq *= 2.0;
+			}
+			return curr_sample;
+		}
+
+		void SetOctaves(int o)		{octaves = o;}
+		void SetFrequency(double f)	{frequency = f;}
+		void SetAmplitude(double a)	{amplitude = a;}
+
+		PerlinNoise2d(){makePermutation();}
+};
+
+#endif
