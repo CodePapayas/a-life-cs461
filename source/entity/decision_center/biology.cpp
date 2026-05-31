697cb1d7ae9ce1679bd8d0851e56da68da1fd950
CodePapayas <catspianos@gmail.com>
2026-05-31 16:45:36 -0700

    adjust entity energy drain settings to be more permissive and encourage longer life

--- source/entity/decision_center/biology.cpp
+++ source/entity/decision_center/biology.cpp
@@ -197,7 +197,7 @@ double Biology::movement_energy_drain(const std::string& terrain_type)
 
     double amount = std::max(
         efficiency * TERRAIN_ENERGY_COEFFICIENT * _genetic_values["Mass"],
-        0.01
+        0.001
     );
 
     add_energy(amount * -1);
@@ -225,7 +225,7 @@ double Biology::movement_water_drain(const std::string& terrain_type)
     double water_efficiency = 1.0 - _genetic_values["Water Efficiency"];
     double amount = std::max(
         water_efficiency * efficiency * TERRAIN_WATER_COEFFICIENT,
-        0.01
+        0.001
     );
 
     add_water(amount * -1);
@@ -253,7 +253,7 @@ double Biology::tick_energy_drain()
     // Calculate the drain: sqrt of sum, divided by number of traits, adjusted for mass
     total = std::pow(total, 0.5) / static_cast<double>(_genetic_values.size());
     total = total * (1.0 - std::pow(_genetic_values["Mass"], 2.0));
-    double drain = std::max(total * ENERGY_DRAIN_COEFFICIENT,.02);
+    double drain = std::max(total * ENERGY_DRAIN_COEFFICIENT,.01);
     add_energy(drain * -1);
     return total;
 }
@@ -268,7 +268,7 @@ double Biology::tick_health_drain()
     if (_energy < .2)
     {
         double difference = _genetic_values["Mass"] - _energy;
-        double drain = std::pow(difference, 2.0);
+        double drain = std::pow(difference, 2.0) * 0.1;
         add_health(drain * -1);
         return drain;
     }
